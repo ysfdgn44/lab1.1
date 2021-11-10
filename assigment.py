9ecea2d58434e96b1c55ddebebcae6585c54f766
yusuf <Bayram@DESKTOP>
2021-11-10 14:54:55 +0300

    assigment.py

---
+++ assigment.py
@@ -0,0 +1,7 @@
+name = 'yusuf'
+pasword = 'ysf44dgn'
+your_name = str(input('give your name : ')
+if name == your_name :
+	print ('welcome! Your password : ', pasword)
+else:
+	print ('Try again. See you later.')
